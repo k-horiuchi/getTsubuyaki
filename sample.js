19d051dda767b42f16f868e0cf58c14eb9f0131c
k-horiuchi <anomer2@gmail.com>
2019-06-29 10:47:37 +0900

    update sample.js

--- sample.js
+++ sample.js
@@ -1,7 +1,13 @@
-const test = document.createElement('a');
-test.href = 'data:text/plain,' + encodeURIComponent(
-    document.querySelector("#permalink-overlay-dialog").innerHTML.toString());
-test.download = 'test.txt';
-test.style.display = 'none';
-document.body.appendChild(test);
-test.click();
+javascript:
+const deleteTarget = ["ul",".js-first-tabstop","button",".stream-end-inner",".flex-module-header",".promoted-heading",".ProfileTweet-actionCountForAria",".t1-form.tweet-form.condensed.is-reply",".self-thread-permalink-cta"];
+deleteTarget.forEach(e => document.querySelectorAll(e).forEach(e => e.remove()));
+const updateTarget = {"a":"href",".AdaptiveMedia-singlePhoto":"style",".AdaptiveMedia-photoContainer":"style"};
+Object.keys(updateTarget).forEach(key => document.querySelectorAll(key).forEach(e => e.removeAttribute(updateTarget[key])));
+document.querySelectorAll("span.js-short-timestamp").forEach(e => e.innerText = "\r\n" + e.innerText);
+const html = document.createElement('a');
+html.href = 'data:text/plain,' + encodeURIComponent(document.querySelector("#permalink-overlay-dialog").innerHTML.toString());
+html.download = 'gyotaku.html';
+html.style.display = 'none';
+document.body.appendChild(html);
+html.click();
+location.reload();
